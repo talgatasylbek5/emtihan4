094acee93ff4170488d01a7caba4a50625c3cd5d
talgatasylbek5 <talgatasylbek5@gmail.com>
2024-11-11 17:59:01 +0600

    Add files via upload

---
+++ vip.js
@@ -0,0 +1,20 @@
+var number 
+var isRain = prompt("көктем");
+
+  if (isRain == '3,4,5'|| isRain == '3,4,5'){
+      if ('жаз');
+  }else if (isRain == '6,7,8' || isRain== '6,7,8'){
+
+  }
+      var isMorninq = prompt("күз");{
+      }
+      
+      if (isMorninq == '9,10,11' || isMorninq == '9,10,11'){
+          alert('қыс');
+      }else if(isMorninq == '12,1,2' || isMorninq == '12,1,2'){
+      }
+
+
+
+
+     
